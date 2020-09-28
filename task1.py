3861f07d7f5fe1c511d5a4f5787adfebd135cf01
Gabriel-03 <71287702+Gabriel-03@users.noreply.github.com>
2020-09-28 12:17:28 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -11,3 +11,10 @@
 # "The number is larger than 100"
 # "The number is smaller than 100"
 # "The number is 100"
+a = int(input("what number"))
+if a < 100 :
+    print ("number less than 100")
+if a > 100 :
+    print("number more than 100")
+if a == 100 :
+    print ("number is 100")
